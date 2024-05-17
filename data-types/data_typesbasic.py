4d12ca93644628a2bb83d726eb60fa90b4245212
damandeep611 <damandeep611@gmail.com>
2024-05-17 15:56:35 +0530

    created data types file and code to practice

---
+++ data-types/data_typesbasic.py
@@ -0,0 +1,39 @@
+
+# numbers data types in python 
+shares_owned = 100
+stock_price = 54.32 
+market_index = 30500.75 
+
+shares_value = shares_owned * stock_price
+print(shares_value)
+
+# String data type 
+stock_symbol = 'AAPL'
+stock_name = 'Apple Inc.'
+stock_message = "thank you for investing in apple inc"
+print(stock_message)
+
+# lists data types 
+
+portfolio = ['APPL', 'GOOGL', 'MSFT', 'AMZN']
+print(portfolio[2])
+
+# Tuples data types
+stock_data = ('2022-01-01', 150.25, 10000)
+print(stock_data)
+
+# Dictionaries
+stock_prices = {'AAPL': 150.25, 'GOOGL': 2800.75, 'MSFT': 300.50 }
+print(stock_prices['AAPL'])
+
+# Sets data types
+unique_stocks = {'AAPL', 'GOOGL', 'SBILIFE'}
+
+# Boolean 
+price_increased = True
+
+# none - it represents abasence of value 
+stock_dividend = None
+
+
+
