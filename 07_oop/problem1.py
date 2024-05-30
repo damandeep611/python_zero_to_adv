95e6fb4166c19ead4ce866a4856dd0203b9eed94
damandeep611 <damandeep611@gmail.com>
2024-05-30 13:16:34 +0530

    OOP problem solving

--- 07_oop/problem1.py
+++ 07_oop/problem1.py
@@ -1,15 +1,48 @@
 class Car:
+  total_car = 0
+
+
   def __init__(self, brand, model):
     self.brand = brand
     self.model = model
+    Car.total_car += 1
+
+  def get_brand(self):
+    return self.brand +  "!"
 
   def full_name(self):
     return f"{self.brand} {self.model}"
 
-my_car = Car(input("Enter car brand:"), input("Enter Car Model:"))
-print("Car Brand:", my_car.brand)
-print("Car Model:", my_car.model)
-print(my_car.full_name())
+  def fuel_type(self):
+    return "Petrol or Diesel"
+  
+  @staticmethod
+  def general_info():
+    return "Cars are fucking amazing"
 
-my_new_car = Car("Tata", "Safari")
-print(my_new_car.model)
+class ElectricCar(Car):
+  def __init__(self, brand, model, battery_size):
+    super().__init__(brand, model)
+    self.battery_size = battery_size
+
+  def fuel_type(self):
+    return "Electric Charge"
+
+# my_tesla = ElectricCar("Tesla", "Model S", "85kWh")
+# print(my_tesla.brand)
+# print(my_tesla.fuel_type())
+
+my_car = Car("Tata", "Safari")
+
+Car("Tata", "Nexon")
+
+# print(my_car.general_info())
+print(Car.general_info())
+
+# my_car = Car(input("Enter car brand:"), input("Enter Car Model:"))
+# print("Car Brand:", my_car.brand)
+# print("Car Model:", my_car.model)
+# print(my_car.full_name())
+
+# my_new_car = Car("Tata", "Safari")
+# print(my_new_car.model)
